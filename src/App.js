3c3dbbce77633482caa0b3104d8f1a33aae8d2ab
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-04-03 18:24:45 -0300

    css navbar and  footer

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 
+//COMPONENTS
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter> 
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-          </Routes>
-        </div>
+        <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <Footer className={styles.footer}>
+        <h3>Escreva sobre o que voce tem interesse!</h3>
+        <p>Mini Blog &copy; 2023</p>
+    </Footer>
+  )
+}
+
+export default Footer
