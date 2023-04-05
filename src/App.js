6c6e676b5f4f44f83d882dd3f496bb4d6dc48a20
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-04-05 18:23:39 -0300

    authenticated user link

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 //CONTEXT
 import { AuthProvider } from './context/AuthContext';
@@ -49,6 +51,8 @@ function App() {
                 <Route path='/about' element={<About />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/posts/create' element={<CreatePost />} />
+                <Route path='/dashboard' element={<Dashboard />} />
               </Routes>
             </div>
           <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,85 @@
 import { Link } from "react-router-dom"
 
+import { useAuthentication } from "../hooks/useAuthentication";
+
+import { useAuthValue } from "../contexts/AuthContext";
+
 import styles from "./Navbar.module.css"
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-        <Link to="/" className={styles.brand}>
-            Mini <span>Blog</span>
-        </Link>
+    const { logout } = useAuthentication();
+    const { user } = useAuthValue();
+  
+    return (
+      <nav className={styles.navbar}>
+        <NavLink className={styles.brand} to="/">
+          Mini <span>Blog</span>
+        </NavLink>
         <ul className={styles.links_list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          {!user && (
+            <>
+              <li>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) => (isActive ? styles.active : "")}
+                >
+                  Entrar
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/register"
+                  className={({ isActive }) => (isActive ? styles.active : "")}
+                >
+                  Cadastrar
+                </NavLink>
+              </li>
+            </>
+          )}
+          {user && (
+            <>
+              <li>
+                <NavLink
+                  to="/posts/create"
+                  className={({ isActive }) => (isActive ? styles.active : "")}
+                >
+                  Novo post
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dashboard"
+                  className={({ isActive }) => (isActive ? styles.active : "")}
+                >
+                  Dashboard
+                </NavLink>
+              </li>
+            </>
+          )}
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Sobre
+            </NavLink>
+          </li>
+          {user && (
             <li>
-                <Link to="/" className={({isActive}) => (isActive ? styles.active : "")}>Home</Link>
+              <button onClick={logout}>Sair</button>
             </li>
-            <li>
-                <Link to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Entrar</Link>
-            </li>
-            <li>
-                <Link to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</Link>
-            </li>
-            <li>
-                <Link to="/about" className={({isActive}) => (isActive ? styles.active : "")}>Sobre</Link>
-            </li>
+          )}
         </ul>
-    </nav>
-  )
-}
-
-export default Navbar
+      </nav>
+    );
+  };
+  
+  export default Navbar;
